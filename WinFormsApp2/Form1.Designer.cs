c9783f372919bbe00730a91a527664d1411c26fc
fokins <sergejfokin829@gmail.com>
2021-10-29 14:06:11 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GateIoTrxTextBox = new System.Windows.Forms.TextBox();
+            this.TrxLabel = new System.Windows.Forms.Label();
+            this.GateIoXtzTextBox = new System.Windows.Forms.TextBox();
+            this.XtzLabel = new System.Windows.Forms.Label();
+            this.GateIoAdaTextBox = new System.Windows.Forms.TextBox();
+            this.AdaLabel = new System.Windows.Forms.Label();
+            this.GateIoDotTextBox = new System.Windows.Forms.TextBox();
+            this.DotLabel = new System.Windows.Forms.Label();
+            this.GateIoLinkTextBox = new System.Windows.Forms.TextBox();
+            this.LinkLabel = new System.Windows.Forms.Label();
+            this.KrakenLinkTextBox = new System.Windows.Forms.TextBox();
+            this.KrakenDotTextBox = new System.Windows.Forms.TextBox();
+            this.KrakenAdaTextBox = new System.Windows.Forms.TextBox();
+            this.KrakenXtzTextBox = new System.Windows.Forms.TextBox();
+            this.KrakenTrxTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // GateIoTrxTextBox
+            // 
+            this.GateIoTrxTextBox.Location = new System.Drawing.Point(12, 405);
+            this.GateIoTrxTextBox.Multiline = true;
+            this.GateIoTrxTextBox.Name = "GateIoTrxTextBox";
+            this.GateIoTrxTextBox.Size = new System.Drawing.Size(353, 22);
+            this.GateIoTrxTextBox.TabIndex = 19;
+            // 
+            // TrxLabel
+            // 
+            this.TrxLabel.AutoSize = true;
+            this.TrxLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TrxLabel.Location = new System.Drawing.Point(12, 381);
+            this.TrxLabel.Name = "TrxLabel";
+            this.TrxLabel.Size = new System.Drawing.Size(37, 21);
+            this.TrxLabel.TabIndex = 18;
+            this.TrxLabel.Text = "TRX";
+            // 
+            // GateIoXtzTextBox
+            // 
+            this.GateIoXtzTextBox.Location = new System.Drawing.Point(12, 312);
+            this.GateIoXtzTextBox.Multiline = true;
+            this.GateIoXtzTextBox.Name = "GateIoXtzTextBox";
+            this.GateIoXtzTextBox.Size = new System.Drawing.Size(353, 22);
+            this.GateIoXtzTextBox.TabIndex = 17;
+            // 
+            // XtzLabel
+            // 
+            this.XtzLabel.AutoSize = true;
+            this.XtzLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.XtzLabel.Location = new System.Drawing.Point(12, 288);
+            this.XtzLabel.Name = "XtzLabel";
+            this.XtzLabel.Size = new System.Drawing.Size(36, 21);
+            this.XtzLabel.TabIndex = 16;
+            this.XtzLabel.Text = "XTZ";
+            // 
+            // GateIoAdaTextBox
+            // 
+            this.GateIoAdaTextBox.Location = new System.Drawing.Point(12, 219);
+            this.GateIoAdaTextBox.Multiline = true;
+            this.GateIoAdaTextBox.Name = "GateIoAdaTextBox";
+            this.GateIoAdaTextBox.Size = new System.Drawing.Size(353, 22);
+            this.GateIoAdaTextBox.TabIndex = 15;
+            // 
+            // AdaLabel
+            // 
+            this.AdaLabel.AutoSize = true;
+            this.AdaLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AdaLabel.Location = new System.Drawing.Point(12, 195);
+            this.AdaLabel.Name = "AdaLabel";
+            this.AdaLabel.Size = new System.Drawing.Size(41, 21);
+            this.AdaLabel.TabIndex = 14;
+            this.AdaLabel.Text = "ADA";
+            // 
+            // GateIoDotTextBox
+            // 
+            this.GateIoDotTextBox.Location = new System.Drawing.Point(12, 126);
+            this.GateIoDotTextBox.Multiline = true;
+            this.GateIoDotTextBox.Name = "GateIoDotTextBox";
+            this.GateIoDotTextBox.Size = new System.Drawing.Size(353, 22);
+            this.GateIoDotTextBox.TabIndex = 13;
+            // 
+            // DotLabel
+            // 
+            this.DotLabel.AutoSize = true;
+            this.DotLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DotLabel.Location = new System.Drawing.Point(12, 102);
+            this.DotLabel.Name = "DotLabel";
+            this.DotLabel.Size = new System.Drawing.Size(40, 21);
+            this.DotLabel.TabIndex = 12;
+            this.DotLabel.Text = "DOT";
+            // 
+            // GateIoLinkTextBox
+            // 
+            this.GateIoLinkTextBox.Location = new System.Drawing.Point(12, 33);
+            this.GateIoLinkTextBox.Multiline = true;
+            this.GateIoLinkTextBox.Name = "GateIoLinkTextBox";
+            this.GateIoLinkTextBox.Size = new System.Drawing.Size(353, 22);
+            this.GateIoLinkTextBox.TabIndex = 11;
+            // 
+            // LinkLabel
+            // 
+            this.LinkLabel.AutoSize = true;
+            this.LinkLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LinkLabel.Location = new System.Drawing.Point(12, 9);
+            this.LinkLabel.Name = "LinkLabel";
+            this.LinkLabel.Size = new System.Drawing.Size(43, 21);
+            this.LinkLabel.TabIndex = 10;
+            this.LinkLabel.Text = "LINK";
+            // 
+            // KrakenLinkTextBox
+            // 
+            this.KrakenLinkTextBox.Location = new System.Drawing.Point(12, 61);
+            this.KrakenLinkTextBox.Multiline = true;
+            this.KrakenLinkTextBox.Name = "KrakenLinkTextBox";
+            this.KrakenLinkTextBox.Size = new System.Drawing.Size(353, 22);
+            this.KrakenLinkTextBox.TabIndex = 20;
+            // 
+            // KrakenDotTextBox
+            // 
+            this.KrakenDotTextBox.Location = new System.Drawing.Point(12, 154);
+            this.KrakenDotTextBox.Multiline = true;
+            this.KrakenDotTextBox.Name = "KrakenDotTextBox";
+            this.KrakenDotTextBox.Size = new System.Drawing.Size(353, 22);
+            this.KrakenDotTextBox.TabIndex = 21;
+            // 
+            // KrakenAdaTextBox
+            // 
+            this.KrakenAdaTextBox.Location = new System.Drawing.Point(12, 247);
+            this.KrakenAdaTextBox.Multiline = true;
+            this.KrakenAdaTextBox.Name = "KrakenAdaTextBox";
+            this.KrakenAdaTextBox.Size = new System.Drawing.Size(353, 22);
+            this.KrakenAdaTextBox.TabIndex = 22;
+            // 
+            // KrakenXtzTextBox
+            // 
+            this.KrakenXtzTextBox.Location = new System.Drawing.Point(12, 340);
+            this.KrakenXtzTextBox.Multiline = true;
+            this.KrakenXtzTextBox.Name = "KrakenXtzTextBox";
+            this.KrakenXtzTextBox.Size = new System.Drawing.Size(353, 22);
+            this.KrakenXtzTextBox.TabIndex = 23;
+            // 
+            // KrakenTrxTextBox
+            // 
+            this.KrakenTrxTextBox.Location = new System.Drawing.Point(12, 433);
+            this.KrakenTrxTextBox.Multiline = true;
+            this.KrakenTrxTextBox.Name = "KrakenTrxTextBox";
+            this.KrakenTrxTextBox.Size = new System.Drawing.Size(353, 22);
+            this.KrakenTrxTextBox.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 505);
+            this.Controls.Add(this.KrakenTrxTextBox);
+            this.Controls.Add(this.KrakenXtzTextBox);
+            this.Controls.Add(this.KrakenAdaTextBox);
+            this.Controls.Add(this.KrakenDotTextBox);
+            this.Controls.Add(this.KrakenLinkTextBox);
+            this.Controls.Add(this.GateIoTrxTextBox);
+            this.Controls.Add(this.TrxLabel);
+            this.Controls.Add(this.GateIoXtzTextBox);
+            this.Controls.Add(this.XtzLabel);
+            this.Controls.Add(this.GateIoAdaTextBox);
+            this.Controls.Add(this.AdaLabel);
+            this.Controls.Add(this.GateIoDotTextBox);
+            this.Controls.Add(this.DotLabel);
+            this.Controls.Add(this.GateIoLinkTextBox);
+            this.Controls.Add(this.LinkLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox GateIoTrxTextBox;
+        private System.Windows.Forms.Label TrxLabel;
+        private System.Windows.Forms.TextBox GateIoXtzTextBox;
+        private System.Windows.Forms.Label XtzLabel;
+        private System.Windows.Forms.TextBox GateIoAdaTextBox;
+        private System.Windows.Forms.Label AdaLabel;
+        private System.Windows.Forms.TextBox GateIoDotTextBox;
+        private System.Windows.Forms.Label DotLabel;
+        private System.Windows.Forms.TextBox GateIoLinkTextBox;
+        private System.Windows.Forms.Label LinkLabel;
+        private System.Windows.Forms.TextBox KrakenLinkTextBox;
+        private System.Windows.Forms.TextBox KrakenDotTextBox;
+        private System.Windows.Forms.TextBox KrakenAdaTextBox;
+        private System.Windows.Forms.TextBox KrakenXtzTextBox;
+        private System.Windows.Forms.TextBox KrakenTrxTextBox;
+    }
+}
+
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExchangeSharp;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var task = Task.Run(async () => {
+                while (true)
+                {
+                    List<string> GateIoCoinNames = new List<string>() { "link_usdt", "dot_usdt", "ada_usdt", "xtz_usdt", "trx_usdt" };
+                    List<string> KrakenCoinNames = new List<string>() { "LINKUSD", "DOTUSD", "ADAUSD", "XTZUSD", "TRXUSD" };
+
+                    List<string> ExchangeNames = new List<string>() { ExchangeName.GateIo, ExchangeName.Kraken };
+
+                    Dictionary<string, List<string>> CoinNames = new Dictionary<string, List<string>>();
+
+                    CoinNames[ExchangeName.GateIo] = GateIoCoinNames;
+                    CoinNames[ExchangeName.Kraken] = KrakenCoinNames;
+
+                    foreach(var ExchangeName in ExchangeNames)
+                    {
+                        var CurExchangeAPI = await ExchangeAPI.GetExchangeAPIAsync(ExchangeName);
+
+                        for(int i = 0; i < CoinNames[ExchangeName].Count; i++)
+                        {
+                            var CurExchangeTicker = await CurExchangeAPI.GetTickerAsync(CoinNames[ExchangeName][i]);
+
+                            UpdateTextBox($"{ExchangeName} bid: {CurExchangeTicker.Bid} ask: {CurExchangeTicker.Ask}", ExchangeName, i);
+                        }
+                    }
+
+                    //Thread.Sleep(100);
+
+
+                    /*
+                    var GateIoApi = await ExchangeAPI.GetExchangeAPIAsync(ExchangeName.GateIo);
+
+                    List<string> GateIoPrices = new List<string>();
+
+                    foreach(var GateIoCoinName in GateIoCoinNames)
+                    {
+                        var GateIoTicker = await GateIoApi.GetTickerAsync(GateIoCoinName);
+                        GateIoPrices.Add($"GateIo {GateIoTicker.MarketSymbol} bid: {GateIoTicker.Bid} ask: {GateIoTicker.Ask}");
+                    }
+
+                    var KrakenApi = await ExchangeAPI.GetExchangeAPIAsync(ExchangeName.Kraken);
+
+                    List<string> KrakenPrices = new List<string>();
+
+                    foreach(var KrakenCoinName in KrakenCoinNames)
+                    {
+                        var KrakenTicker = await KrakenApi.GetTickerAsync(KrakenCoinName);
+                        KrakenPrices.Add($"Kraken {KrakenTicker.MarketSymbol} bid: {KrakenTicker.Bid} ask: {KrakenTicker.Ask}");
+                    }
+
+                    UpdateKrakenTextBox(KrakenPrices);
+                    UpdateGateIoTextBox(GateIoPrices);
+
+                    Thread.Sleep(100);
+
+                    */
+                }
+            });
+        }
+
+        public void UpdateTextBox(string value, string TextBoxExchangeName, int TextBoxNum)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string, string, int>(UpdateTextBox), new object[] { value, TextBoxExchangeName, TextBoxNum });
+                return;
+            }
+
+            if(TextBoxExchangeName == ExchangeName.GateIo)
+            {
+                switch (TextBoxNum)
+                {
+                    case 0:
+                        GateIoLinkTextBox.Text = value;
+                        break;
+                    case 1:
+                        GateIoDotTextBox.Text = value;
+                        break;
+                    case 2:
+                        GateIoAdaTextBox.Text = value;
+                        break;
+                    case 3:
+                        GateIoXtzTextBox.Text = value;
+                        break;
+                    case 4:
+                        GateIoTrxTextBox.Text = value;
+                        break;
+                }
+            }else if (TextBoxExchangeName == ExchangeName.Kraken)
+            {
+                switch (TextBoxNum)
+                {
+                    case 0:
+                        KrakenLinkTextBox.Text = value;
+                        break;
+                    case 1:
+                        KrakenDotTextBox.Text = value;
+                        break;
+                    case 2:
+                        KrakenAdaTextBox.Text = value;
+                        break;
+                    case 3:
+                        KrakenXtzTextBox.Text = value;
+                        break;
+                    case 4:
+                        KrakenTrxTextBox.Text = value;
+                        break;
+                }
+            }
+        }
+
+    }
+}
